026e3971b271cca1d229126ffda9cf9ff8939e01
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-09 22:26:25 -0500

    initial routes and base html

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ from stories import story
 app = Flask(__name__)
 
 
+@app.route('/')
+def questions():
+    """Generate and show form to ask words."""
+    prompts = story.prompts
+
+    return render_template('questions.html', prompts=prompts)
+
 
 @app.route('/story')
-def return_story():
-    
+def show_story():
+    """Show story result"""
+    text = story.generate(request.args)
+
+    return render_template('story.html', text=text)
+
