d46a36060a98a1430fe7eb7c786a6f1043e13273
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-09 23:02:56 -0500

    code updates and folder change

--- basic/app.py
+++ basic/app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension
 
 
 @app.route('/')
@@ -16,6 +21,7 @@ def questions():
 @app.route('/story')
 def show_story():
     """Show story result"""
+    
     text = story.generate(request.args)
 
     return render_template('story.html', text=text)
